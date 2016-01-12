704444490b1bb05bc9745a637348b0756cb56470
Memo García <sirmemogarcia@gmail.com>
2016-01-12 11:29:17 +0000

    Initial setup

---
+++ api.js
@@ -0,0 +1,26 @@
+// NC-API main
+
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'NC-API' });
+});
+
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+app.listen(port);
+console.log('Running on port ' + port);
